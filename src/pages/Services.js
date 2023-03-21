4ba8314ede5c55b9c9eed847ea39c540f4d35b1b
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-03-21 15:39:20 +0600

    Best way of nested route

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import AppDevelopment from "../components/AppDevelopment";
-import WebDevelopment from "../components/WebDevelopment";
+import { Outlet } from "react-router-dom";
 
 const Services = () => {
   return (
     <div>
       <h1>Services Page</h1>
-      <Routes>
-        <Route path="web-dev" element={<WebDevelopment />} />
-        <Route path="app-dev" element={<AppDevelopment />} />
-      </Routes>
+      <Outlet />
     </div>
   );
 };
